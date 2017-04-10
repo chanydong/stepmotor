c2a88aa58d695fcce83e622c8b2c92aabac7a33a
chanydong <chanydong325@gmail.com>
2017-04-10 12:54:40 +0900

    add function

--- step_motor/motor00.c
+++ step_motor/motor00.c
@@ -6,21 +6,43 @@
 #define B	24
 #define BB	25
 
+#define TRIG	28
+#define ECHO	29
+
 #define SW	21
-#define TIME	800
+#define TIME	100
+
+///////////////////////////FUNCTION/////////////////////////////
 void initialize(void);
+void iris_reset(int a, int b, int c, int d, int e, int f);
+
 void iris_control(int a, int b, int c, int d);
 
 void iris_open(void);
 void iris_close(void);
+////////////////////////////////////////////////////////////////
 
 void initialize(void)
 {
+	pinMode(TRIG,OUTPUT);
+	pinMode(ECHO,INPUT);	
+
 	pinMode(A,OUTPUT);
 	pinMode(AA,OUTPUT);
 	pinMode(B,OUTPUT);
 	pinMode(BB,OUTPUT);
-	pinMode(SW,INPUT);	
+	pinMode(SW,INPUT);
+
+	iris_reset(0,0,0,0,0,0);	
+}
+void iris_reset(int a, int b, int c, int d, int e, int f)
+{
+	digitalWrite(A,a);
+	digitalWrite(AA,b);
+	digitalWrite(B,c);
+	digitalWrite(BB,d);
+	digitalWrite(TRIG,e);
+	digitalWrite(ECHO,f);
 }
 
 void iris_control(int a, int b, int c, int d)
@@ -63,12 +85,14 @@ int main(void)
 	{
 		if(digitalRead(SW) == 1)
 		{
+		        printf("open\n");
 			iris_open();
-		        printf("success\n");
 		}
 		else
+		{
+		        printf("close\n");
 			iris_close();
-
+		}
 	}
 
 	return 0;
